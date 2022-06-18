d91bf6cb7ce4279124b5c63f925e4f4b1207df90
jimmyblackwood576@outlook.com <jimmyblackwood576@outlook.com>
2022-06-18 22:58:20 +0300

    Добавьте файлы проекта.

---
+++ HomeTask_3.0.4/HomeTask_3.0.4.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "ru");
+
+    int cellsize,a=1,b=1,c=1;
+    cout << "Укажите размер клетки шахматной доски -> ";
+    cin >> cellsize;
+    while(c<=4) {
+        while (a <= cellsize) {
+            cout << string(cellsize, '_') << string(cellsize, '*')
+                << string(cellsize, '_') << string(cellsize, '*')
+                << string(cellsize, '_') << string(cellsize, '*')
+                << string(cellsize, '_') << string(cellsize, '*')<<endl ;
+            a++;
+        }
+        
+        while (b <= cellsize) {
+            cout << string(cellsize, '*') << string(cellsize, '_')
+                << string(cellsize, '*') << string(cellsize, '_')
+                << string(cellsize, '*') << string(cellsize, '_')
+                << string(cellsize, '*') << string(cellsize, '_')<<endl ;
+            b++;
+        }
+            
+       
+        c++;
+        a=1,b = 1;
+    }
+}
+
